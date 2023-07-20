ea3c6360d17cd3d89fd9024b24074aeeb32a8f1d
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-07-20 22:07:16 +0300

    fix bg

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react' 
+import Hero from './hero/Hero';
 
 
 const Home = () => {
   return (
-    <div>
-     <h1>hello</h1>
-    </div>
+    <>
+     
+     <Hero/>
+    </>
   )
 }
 
