391ec6319f64e0ce8dccb76690a0c582a067d757
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-07-21 14:25:00 +0300

    file structure

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './components/home/Home';
+import Home from './pages/Home/Home';
 
 
 const UserRoutes = () => {
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react' 
-import Hero from './hero/Hero';
+import Hero from "./Hero/Hero"
 
 
 const Home = () => {
