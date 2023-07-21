b5f07779f05fd7958a2cf6c04606871a6715ef90
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-07-21 22:55:11 +0300

    calls

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Calls from './pages/Calls/Calls';
 
 
 const UserRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/calls" element={<Calls />} />
       <Route path="*" element={<Home />} />
     </Routes>
   );
---
+++ src/pages/Calls/Calls.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Back from '../../components/common/Back/Back'
+
+const Calls = () => {
+  return (
+    <>
+    
+      <Back title="Розклад дзвоників"/>
+    </>
+  )
+}
+
+export default Calls
