d72b5f591d8474fc39f19f3e243091e83fae1fbd
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-07-27 22:08:31 +0300

    input tel

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Back from "../../components/Back/Back";
 import "./contacts.css";
 
 const Contacts = () => {
+  const [message, setMessage] = useState("");
   const map =
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2563.653419833835!2d36.340143576126145!3d50.01785091841813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x412709dea72f2a97%3A0x324e0d27ba282882!2z0KXQsNGA0LrRltCy0YHRjNC60LAg0LfQsNCz0LDQu9GM0L3QvtC-0YHQstGW0YLQvdGPINGI0LrQvtC70LAgSeKAk0lJSSDRgdGC0YPQv9C10L3RltCyIOKEliAxNDA!5e0!3m2!1suk!2sua!4v1690457055281!5m2!1suk!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"';
-    const phoneNumber = "+380577251651";
-    const formSpreeEndpoint="https://formspree.io/f/xrgwezpa"
-    return (
+  const phoneNumber = "+380577251651";
+  const formSpreeEndpoint = "https://formspree.io/f/xrgwezpa";
+
+  const handleTextareaFocus = () => {
+    if (message === "Напишіть ваше повідомлення...") {
+      setMessage("");
+    }
+  };
+
+  const handleTextareaBlur = () => {
+    if (message.trim() === "") {
+      setMessage("Напишіть ваше повідомлення...");
+    }
+  };
+  return (
     <>
       <Back title="Наші контакти" />
       <section className="contacts padding">
@@ -16,7 +29,9 @@ const Contacts = () => {
             <iframe src={map} title="Наш заклад на Google картах"></iframe>
           </div>
           <div className="contacts-right row">
-            <h1 className="contacts-title">Контакти КЗ "Харківський ліцей № 140" Харківської міськох ради</h1>
+            <h1 className="contacts-title">
+              Контакти КЗ "Харківський ліцей № 140" Харківської міськох ради
+            </h1>
             <ul className="items grid3">
               <li className="box">
                 <h4>Адреса:</h4>
@@ -34,14 +49,47 @@ const Contacts = () => {
 
             <form action={formSpreeEndpoint} method="POST">
               <div className="flexSB">
-                <input type="text" name="name"  placeholder="Прізвище Ім'я" required />
-                <input type="email" name="_replyto" placeholder="Ваша елнектронна пошта" required/>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Прізвище Ім'я"
+                  required
+                />
+                <input
+                  type="email"
+                  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+                  name="_replyto"
+                  placeholder="Ваша електронна пошта"
+                  required
+                />
               </div>
-              <input type="text" name="subject"  placeholder="Тема повідомлення" required/>
-              <textarea name="message" cols="30" rows="10" required>
-                Напишіть ваше повідомлення...
-              </textarea>
-              <button type="submit" className="primary-btn">Відправити повідомлення</button>
+              <input
+                type="text"
+                name="subject"
+                placeholder="Тема повідомлення"
+                required
+              />
+                 <input
+                type="tel"
+                name="phone"
+                placeholder="Ваш телефон у форматі +380ХХХХХХХХХ"
+                pattern="^\+380\d{9}$"
+                title="Введіть номер телефону у форматі +380ХХХХХХХХХ "
+                required
+              />
+              <textarea
+                name="message"
+                cols="30"
+                rows="10"
+                required
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onFocus={handleTextareaFocus}
+                onBlur={handleTextareaBlur}
+              ></textarea>
+              <button type="submit" className="primary-btn">
+                Відправити повідомлення
+              </button>
             </form>
 
             <h3>Приєднуйтесь до нас у соціальній мережі</h3>
