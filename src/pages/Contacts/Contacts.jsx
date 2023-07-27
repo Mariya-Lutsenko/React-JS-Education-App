b6707dfa75c3df4c067b9f6076f7d059431bb3de
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-07-27 21:43:13 +0300

    formSpree

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -6,6 +6,7 @@ const Contacts = () => {
   const map =
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2563.653419833835!2d36.340143576126145!3d50.01785091841813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x412709dea72f2a97%3A0x324e0d27ba282882!2z0KXQsNGA0LrRltCy0YHRjNC60LAg0LfQsNCz0LDQu9GM0L3QvtC-0YHQstGW0YLQvdGPINGI0LrQvtC70LAgSeKAk0lJSSDRgdGC0YPQv9C10L3RltCyIOKEliAxNDA!5e0!3m2!1suk!2sua!4v1690457055281!5m2!1suk!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"';
     const phoneNumber = "+380577251651";
+    const formSpreeEndpoint="https://formspree.io/f/xrgwezpa"
     return (
     <>
       <Back title="Наші контакти" />
@@ -31,16 +32,16 @@ const Contacts = () => {
               </li>
             </ul>
 
-            <form action="">
+            <form action={formSpreeEndpoint} method="POST">
               <div className="flexSB">
-                <input type="text" placeholder="Прізвище Ім'я" />
-                <input type="email" placeholder="Ваша елнектронна пошта" />
+                <input type="text" name="name"  placeholder="Прізвище Ім'я" required />
+                <input type="email" name="_replyto" placeholder="Ваша елнектронна пошта" required/>
               </div>
-              <input type="text" placeholder="Тема повідомлення" />
-              <textarea cols="30" rows="10">
+              <input type="text" name="subject"  placeholder="Тема повідомлення" required/>
+              <textarea name="message" cols="30" rows="10" required>
                 Напишіть ваше повідомлення...
               </textarea>
-              <button className="primary-btn">Відправити повідомлення</button>
+              <button type="submit" className="primary-btn">Відправити повідомлення</button>
             </form>
 
             <h3>Приєднуйтесь до нас у соціальній мережі</h3>
