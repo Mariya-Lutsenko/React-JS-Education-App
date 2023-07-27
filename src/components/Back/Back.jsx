649dada2bc2dc427818d5a88e2f6081b7c8b5b01
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-07-27 23:12:03 +0300

    fix

--- src/components/Back/Back.jsx
+++ src/components/Back/Back.jsx
@@ -8,7 +8,7 @@ const Back = ({title}) => {
   
     <>
       <section className='back'>
-        <h2>Home / {location.pathname.split("/")[1]}</h2>
+
         <h1>{title}</h1>
         <Clouds/>
       </section>
