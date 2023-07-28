77f0ef963cae1510fd372c46ecdcce97f02fa953
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-07-28 20:26:20 +0300

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="container footer-padding">
+        <div className="legal">
+        <p>
+          &#169; 2023 | All Rights Reserved | Developed with
+          <span className="footer__icon">
+            <i className="fa fa-heart"></i>
+          </span>
+          by Mariia Lutsenko
+        </p>
+      </div>
+        </div>
+      </footer>
+      
+    </>
+  );
+};
+
+export default Footer;
