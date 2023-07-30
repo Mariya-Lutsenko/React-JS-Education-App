0e44eeb016427055ecd1608b6595ee96cad20e57
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-07-30 14:47:08 +0300

    lazy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-// import { Suspense } from "react";
+import { Suspense } from "react";
 import "./App.css";
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Header from "./components/Heading/Header";
 import UserRoutes from "./UserRoutes";
 import Footer from "./components/Footer/Footer";
-// import Loader from "./components/Loader/Loader";
+import Loader from "./components/Loader/Loader";
 
 const App = () => {
   const location = useLocation();
   const isHomePage = location.pathname === "/";
   return (
     <d>
-      {/* <Suspense fallback={<Loader />}> */}
+      <Suspense fallback={<Loader />}>
       <Header />
       <UserRoutes />
       {!isHomePage && <Footer />}
 
-      {/* </Suspense> */}
+      </Suspense>
     </d>
   );
 };
